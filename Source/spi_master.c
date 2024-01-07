97211b73e420ea829d64a54ac0717261f80ff5f9
Albert Muller <jzxdrift@users.noreply.github.com>
2024-01-07 15:51:03 -0800

    Update and rename spi_master.c to Source/spi_master.c

--- Source/spi_master.c
+++ Source/spi_master.c
@@ -3,8 +3,8 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define SS			2
-#define SCK			5
+#define SS		2
+#define SCK		5
 
 #define BYTE		8
 
@@ -13,7 +13,7 @@ static uint8_t receiveSPIMaster(void);
 void initSPIMaster(void)
 {
 	DDRB |= _BV(SCK) | _BV(SS);
-	PORTB |= _BV(SS);												/*pull high to idle if SS is output*/
+	PORTB |= _BV(SS);						/*pull high to idle if SS is output*/
 	
 	/*spi enable
 	master select
@@ -26,16 +26,16 @@ uint16_t readDataSPIMaster(void)
 {
 	uint16_t data = 0;
 	
-	PORTB &= ~_BV(SS);												/*select slave by pulling low*/
+	PORTB &= ~_BV(SS);						/*select slave by pulling low*/
 	data = (receiveSPIMaster() << BYTE) | receiveSPIMaster();
-	PORTB |= _BV(SS);												/*release slave by pulling high*/
+	PORTB |= _BV(SS);						/*release slave by pulling high*/
 	
 	return data;
 }
 
 static uint8_t receiveSPIMaster(void)
 {
-	SPDR = 0x00;													/*fill buffer*/
+	SPDR = 0x00;							/*fill buffer*/
 	loop_until_bit_is_set(SPSR, SPIF);
 	
 	return SPDR;
